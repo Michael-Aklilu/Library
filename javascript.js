906f30312a569c36a4cf9485e4ec98fc5d1292bb
Michael Aklilu <michaelaklilu@gmail.com>
2024-06-02 14:52:15 +0300

    Add logic to clear container when add book is clicked

--- javascript.js
+++ javascript.js
@@ -12,28 +12,9 @@ const book2 = new Book('November 9', 'Coleen Hoover', 'Romance');
 myLibrary.push(book1);
 myLibrary.push(book2);
 
+
 function addBookToLibrary() {
     
-
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    
-    addBookToLibrary()
-    display();
-
-    let button = document.querySelector('#newbook');
-    button.addEventListener('click', () => {
-        let form = document.querySelector('#form').style.display = 'block';
-
-        let closeButton = document.querySelector('#closebutton');
-        closeButton.addEventListener('click', () => {
-            let form = document.querySelector('#form').style.display = 'none';
-        })
-    });
-    let submit = document.querySelector("#submitbutton");
-    submit.addEventListener("click",()=>{
         let bookInput = document.querySelector('#book');
         let authorInput = document.querySelector('#author');
         let genreInput = document.querySelector('#genre');
@@ -41,18 +22,44 @@ document.addEventListener("DOMContentLoaded", () => {
         let input = new Book(`${bookInput.value}`, `${authorInput.value}`, `${genreInput.value}`);
         myLibrary.push(input);
 
-    })
-})
+        bookInput.value = "";
+        authorInput.value = "";
+        genreInput.value = "";
 
+        
+        display();
+
+    }
 function display() {
 
-    let container = document.querySelector("#container");
-    for(let book of myLibrary){
+    for (let book of myLibrary) {
         let bookDiv = document.createElement("div");
         bookDiv.innerHTML = `Name: ${book.name}<br>
         Author: ${book.author}<br> Genre: ${book.genre}`;
         container.appendChild(bookDiv);
     }
-
-
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let button = document.querySelector('#newbook');
+    button.addEventListener('click', () => {
+        let form = document.querySelector('#form').style.display = 'block';
+        let clear = document.querySelector('#container');
+        clear.innerHTML = "";
+    })
+
+    let closeButton = document.querySelector('#closebutton');
+    closeButton.addEventListener('click', () => {
+             form = document.querySelector('#form').style.display = 'none';
+        })
+
+    let submitButton = document.querySelector('#submitbutton');
+    submitButton.addEventListener('click', (event)=>{
+        event.preventDefault(); 
+        addBookToLibrary();
+        form.style.display = 'none';
+    })
+        display();
+})
+
