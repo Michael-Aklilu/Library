78f0c43dd200bb6c58db622de4a4b1345d3402b3
Michael Aklilu <michaelaklilu@gmail.com>
2024-06-16 15:09:45 +0300

    Add function to display book in library

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+function Book(name,author,genre){
+    this.name = name;
+    this.author = author;
+    this.genre = genre;
+}
+
+const book1 = new Book('Harry Potter', 'JK Rowling', 'Fantasy');
+const book2 = new Book('Percy Jackson', 'Rick Riordan','Fantasy');
+
+myLibrary.push(book1);
+ myLibrary.push(book2);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    function display(){
+        const container =  document.querySelector('#container');
+    
+        for(let book of myLibrary){
+            let card = document.createElement('div');
+            card.innerHTML = `Book: ${book.name}<br> Author: ${book.author}<br> Genre: ${book.genre}`
+            container.appendChild(card);
+          }
+    } 
+    display();
+})
