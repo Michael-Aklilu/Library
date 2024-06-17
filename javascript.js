aa729820bef35c9b8ddca815d92f1142cd2bce54
Michael Aklilu <michaelaklilu@gmail.com>
2024-06-17 12:23:59 +0300

    Add function to display form and accept input when add button clicked

--- javascript.js
+++ javascript.js
@@ -12,18 +12,55 @@ const book2 = new Book('Percy Jackson', 'Rick Riordan','Fantasy');
 myLibrary.push(book1);
 myLibrary.push(book2);
 
+
+
 document.addEventListener('DOMContentLoaded', ()=>{
     
+    let bName = document.querySelector('#Name');
+    let author = document.querySelector('#Author');
+    let genre = document.querySelector('#Genre');
+    const addBtn = document.querySelector('#Add');
+    const form = document.querySelector('form');
+    const closeBtn = document.querySelector('#closeBtn');
+    const submitBtn = document.querySelector('#submitBtn');
+    const dialog = document.querySelector('dialog');
+    
+   
+   
+    addBtn.addEventListener('click', ()=>{ 
+        dialog.showModal();
+    })
+    closeBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    dialog.close(); 
+    })   
+
+    submitBtn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        addBook();
+        dialog.close();
+        display();
+    })
+   
+    function addBook(){
+      let book = new Book(`${bName.value}`, `${author.value}`, `${genre.value}`);
+      myLibrary.push(book);
+      bName.value = "";
+      author.value = "";
+      genre.value = "";
+    }  
+   
     function display(){
         const container =  document.querySelector('#container');
-    
+        container.innerHTML = ''; 
+        
         for(let book of myLibrary){
             let card = document.createElement('div');
             card.innerHTML = `Book: ${book.name}<br> Author: ${book.author}<br> Genre: ${book.genre} <br>`
-            let addBtn = document.createElement('button');
-            addBtn.textContent = 'Remove';   
+            let removeBtn = document.createElement('button');
+            removeBtn.textContent = 'Remove';   
             container.appendChild(card);
-            card.appendChild(addBtn);
+            card.appendChild(removeBtn);
           }
     } 
     display();
