b6f9326aef7eb0011077a38c86c03a74c628d713
MichaelAklilu <michaelaklilu@gmail.com>
2024-08-05 17:03:11 +0300

    Rewrite code to include class syntax

--- javascript.js
+++ javascript.js
@@ -1,47 +1,50 @@
-const myLibrary = [];
+class Book {
+  static myLibrary = [];
+  constructor(title, genre, author, read) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.read = read;
+  }
+  static addBookToLibrary(title, genre, author, read) {
+    let book = new Book(title, genre, author, read);
+    Book.myLibrary.push(book);
+  }
+  static displayBook() {
+    row.innerHTML = "";
+    Book.myLibrary.forEach((book, index) => {
+      row.innerHTML += `<tr>
+              <td>${book.title}</td> 
+              <td>${book.genre}</td>
+              <td>${book.author}</td>
+              <td>${book.read}</td>
+              <td><button class="removeBtn" data-index = "${index}">Remove</button></td> 
+              <td><button class = "readBtn" data-index = "${index}">Read</button></td>    
+          </tr>
+          `;
+    });
+    const removeBtns = row.querySelectorAll(".removeBtn");
+    const readBtns = row.querySelectorAll(".readBtn");
+    removeBtns.forEach((button) => {
+      button.addEventListener("click", function (event) {
+        let rowIndex = event.target.getAttribute("data-index");
+        Book.myLibrary.splice(rowIndex, 1);
+        Book.displayBook();
+      });
+    });
+    readBtns.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        let rowIndex = event.target.getAttribute("data-index");
+        Book.myLibrary[rowIndex].read =
+          Book.myLibrary[rowIndex].read === "Yes" ? "No" : "Yes";
+        Book.displayBook();
+      });
+    });
+  }
+}
 
-function Book(title, genre, author, read) {
-  this.title = title;
-  this.genre = genre;
-  this.author = author;
-  this.read = read;
-}
-function addBookToLibrary(title, genre, author, read) {
-  let book = new Book(title, genre, author, read);
-  myLibrary.push(book);
-}
 const row = document.querySelector("#addRow");
-function displayBook() {
-  row.innerHTML = "";
-  myLibrary.forEach((book, index) => {
-    row.innerHTML += `<tr>
-        <td>${book.title}</td> 
-        <td>${book.genre}</td>
-        <td>${book.author}</td>
-        <td>${book.read}</td>
-        <td><button class="removeBtn" data-index = "${index}">Remove</button></td> 
-        <td><button class = "readBtn" data-index = "${index}">Read</button></td>    
-    </tr>
-    `;
-  });
-  const removeBtns = row.querySelectorAll(".removeBtn");
-  const readBtns = row.querySelectorAll(".readBtn");
-  removeBtns.forEach((button) => {
-    button.addEventListener("click", function (event) {
-      let rowIndex = event.target.getAttribute("data-index");
-      myLibrary.splice(rowIndex, 1);
-      displayBook();
-    });
-  });
-  readBtns.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      let rowIndex = event.target.getAttribute("data-index");
-      myLibrary[rowIndex].read =
-        myLibrary[rowIndex].read === "Yes" ? "No" : "Yes";
-      displayBook();
-    });
-  });
-}
+
 const newBook = document.querySelector(".newButton");
 const dialog = document.querySelector("dialog");
 const form = document.querySelector("form");
@@ -55,8 +58,8 @@ closeForm.addEventListener("click", () => {
   let genre = document.querySelector("#genre").value;
   let author = document.querySelector("#author").value;
   let read = document.querySelector("#read").value;
-  addBookToLibrary(title, genre, author, read);
+  Book.addBookToLibrary(title, genre, author, read);
   dialog.close();
   form.reset();
-  displayBook();
+  Book.displayBook();
 });
